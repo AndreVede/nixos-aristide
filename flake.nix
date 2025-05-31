55cd9d9303cfcfdfbeaa11207d086feea3683136
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-05-31 13:56:27 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+    description = "My light system to navigate easily. Aristide-on-road";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+  };
+}
