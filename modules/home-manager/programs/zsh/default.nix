8238d453ae52d23ad699a7414211d1bcb6f07666
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-06-26 21:37:28 +0200

    feat: alias add

--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -11,13 +11,14 @@
       ll = "eza -bhl --icons --group-directories-first"; # long list
       la = "eza -abhl --icons --group-directories-first"; # all list
       lt = "eza --tree --level=2 --icons"; # tree
+      ltall = "eza --tree --icons" # all tree
+        };
     };
-  };
 
-  home.file = {
-    # .bashrc, ...
-    ".local/share/zsh/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
-    ".local/share/zsh/zsh-fast-syntax-highlighting".source = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
-    ".local/share/zsh/nix-zsh-completions".source = "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
-  };
-}
+    home.file = {
+      # .bashrc, ...
+      ".local/share/zsh/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
+      ".local/share/zsh/zsh-fast-syntax-highlighting".source = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+      ".local/share/zsh/nix-zsh-completions".source = "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
+    };
+  }
