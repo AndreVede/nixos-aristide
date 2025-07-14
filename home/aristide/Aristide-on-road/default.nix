789866fd927c4c4f5815daf64b615bceeb056bd0
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-07-14 23:07:53 +0200

    feat: dconf init for my usage

--- home/aristide/Aristide-on-road/default.nix
+++ home/aristide/Aristide-on-road/default.nix
@@ -1,6 +1,8 @@
-{ nhModules, ... }:
-{
+{ lib, nhModules, ... }:
 
+with lib.hm.gvariant;
+
+{
   imports = [
     "${nhModules}/common"
     "${nhModules}/desktop/cinnamon"
@@ -12,6 +14,32 @@
     "${nhModules}/programs/kodi"
     "${nhModules}/virtualisation/qemu"
   ];
+
+  dconf.settings = {
+
+    # alimentation
+    "/org/cinnamon/settings-daemon/plugins/power" = {
+      sleep-display-battery = 1800;
+      sleep-display-ac = 1800;
+    };
+
+    # Keyboard shortcuts
+    "org/cinnamon/desktop/keybindings" = {
+      custom-list = [ "custom0" "custom1" "__dummy__" ];
+    };
+
+    "org/cinnamon/desktop/keybindings/custom-keybindings/custom0" = {
+      binding = [ "<Primary><Alt>s" ];
+      command = "waydroid session stop";
+      name = "waydroid stop";
+    };
+
+    "org/cinnamon/desktop/keybindings/custom-keybindings/custom1" = {
+      binding = [ "<Super>g" ];
+      command = "weston";
+      name = "weston";
+    };
+  };
 
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
