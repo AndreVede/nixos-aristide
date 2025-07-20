68fbe0d8e03fdd3b55d1e47c63608f54bf73f77c
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-07-20 12:04:10 +0200

    feat: add a temporary solution for minidlna

--- modules/nixos/services/minidlna/default.nix
+++ modules/nixos/services/minidlna/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, userConfig, hostname, ... }: {
+{ pkgs, userConfig, hostname, lib, ... }: {
   services.minidlna = {
     enable = true;
     settings = {
@@ -8,8 +8,17 @@
       friendly_name = "${hostname} partage !";
       inotify = "yes";
       log_level = "error";
+      db_dir = "/var/cache/minidlna";
+      log_dir = "/var/log";
     };
     openFirewall = true;
+  };
+
+  systemd.services.minidlna.serviceConfig = {
+    ProtectHome = "read-only"; # To be able to read files in home directory
+    # temporary use my user to access /home -> I have to found a better solution with minidlna user.
+    User = lib.mkForce "${userConfig.name}";
+    Group = lib.mkForce "users";
   };
 
   environment.systemPackages = with pkgs; [
