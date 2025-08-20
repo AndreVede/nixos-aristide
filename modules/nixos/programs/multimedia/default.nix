761f4007d626fc5bd74202d063a74ccfcab97a7e
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-08-21 00:56:00 +0200

    feat: set avatar

--- modules/nixos/programs/multimedia/default.nix
+++ modules/nixos/programs/multimedia/default.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  libbluray = pkgs.libbluray.override {
+    withAACS = true;
+    withBDplus = true;
+  };
+  vlc = pkgs.vlc.override { inherit libbluray; };
+in
+{
   environment.systemPackages = with pkgs; [
     # Multimedia
     vlc
