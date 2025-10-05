c2699915e4ba5f9700d1a3d4e750a9ce81055186
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-10-05 10:20:04 +0200

    fix: doc

--- modules/home-manager/programs/steam/default.nix
+++ modules/home-manager/programs/steam/default.nix
@@ -3,7 +3,7 @@
     protonup
   ];
 
-  # run `protonup -d "~/.steam/root/compatibilitytools.d/`"
+  # run `protonup -d "~/.steam/root/compatibilitytools.d/"`
   home.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATH = "\$HOME/.steam/root/compatibilitytools.d";
   };
