b142e0c2e9cac4f4736dfd3661feb2b89898431e
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-10-09 15:51:45 +0200

    feat: java for bluray

--- modules/nixos/programs/multimedia/default.nix
+++ modules/nixos/programs/multimedia/default.nix
@@ -6,10 +6,18 @@ let
     withJava = true;
     libaacs = pkgs.libaacs;
     libbdplus = pkgs.libbdplus;
+    jdk17 = pkgs.jdk17;
+    ant = pkgs.ant;
+    stripJavaArchivesHook = pkgs.stripJavaArchivesHook;
   };
   myVlc = pkgs.vlc.override { inherit libbluray; };
 in
 {
+  programs.java = {
+    enable = true;
+    package = pkgs.jdk17;
+  };
+
   environment.systemPackages = [
     # Multimedia
     myVlc
